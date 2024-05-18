9d0fc0a0c03c85de3ee9cc72f3985acbccb120f4
Serhii Rybalchenko <s.rybalchenko@ukr.net>
2024-05-18 18:38:39 +0200

    upd

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function closeMobMenu() {
+    const closeBtn = document.getElementById("mobile-menu");
+    closeBtn.classList.add("is-close");
+  }
+  function openModal() {
+    const modalWindow = document.getElementById("modalWindow");
+    modalWindow.classList.remove("is-close");
+  }
+  function closeModal() {
+    const modalWindow = document.getElementById("modalWindow");
+    modalWindow.classList.add("is-close");
+  }
+  function openMobMenu() {
+    const closeBtn = document.getElementById("mobile-menu");
+    closeBtn.classList.remove("is-close");
+  }
